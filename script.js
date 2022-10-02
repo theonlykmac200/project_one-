be53298a7c7baaa3f5a43a095c2008e9c9acb582
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-10-02 11:15:51 -0700

    api connected and minor css changes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 // Project Cards 
 //define the cards
-const flipBoxInner = document.querySelector(".flip-box-front")
-const flipBoxInner1 = document.querySelector(".flip-box-front1")
-const flipBoxInner2 = document.querySelector(".flip-box-front2")
-const flipBoxInner3 = document.querySelector(".flip-box-front3")
+const flipBoxInner = document.querySelector(".flip-box-front");
+const flipBoxInner1 = document.querySelector(".flip-box-front1");
+const flipBoxInner2 = document.querySelector(".flip-box-front2");
+const flipBoxInner3 = document.querySelector(".flip-box-front3");
 
 // when someone hovers over the card flip to the back, when they move off of it, change it back to the title
 flipBoxInner.addEventListener('mouseover', function() {
   //change text to backside of the card
-    flipBoxInner.textContent = "A fun game featuring prompts and alerts.  Built in a single object & features Ryan Gossling; who provides postive affermations as players guesses the secret number";
+    flipBoxInner.textContent = "A fun game featuring prompts and alerts.  Built in a single object & features Ryan Gossling; who provides postive affermations as players guesses the secret number";   
 
 });
 flipBoxInner.addEventListener('mouseleave', function() {
@@ -52,27 +52,33 @@ flipBoxInner3.addEventListener('mouseleave', function() {
 );
 
 
-
 // if I have time come back and put in a loop to change out the box contents vs four difference event listeners
 
 const todayQuote = document.getElementById("quote-today");
 const whoSaidIt = document.getElementById("author");
 const refreshBtn = document.getElementById("getNew");
 
+
+
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '760246b516mshaa445d297180d5ap13fc35jsn8abf7cfb6088',
-		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
 	}
 };
 
-fetch('https://famous-quotes4.p.rapidapi.com/random?category=all&count=2', options)
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
 	.then(response => response.json())
-	.then((data) => {
-        todayQuote.innerText = data.text
-        whoSaidIt.innerText = data.author
-        
-    })
-	.catch(err => console.error(err));
+	.then((response) => {
+        console.log(response)
+        // todayQuote.innerText = response.originator[1].name
+        todayQuote.innerText = "Quote of the Day: " + response.content
+        whoSaidIt.innerText = "---" + response.originator.name
+       
 
+    })
+        
+
+
+	.catch(err => console.error(err));
